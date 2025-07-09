c26588c87774d9649ea6765e4d537d7578011f20
Anthony Figueroa <anthony@routestack.net>
2025-07-09 15:52:27 -0700

    Finished P4

--- p4.py
+++ p4.py
@@ -1,20 +1,44 @@
 def main():
-    
+
+    tasks = []
+   
     while True:
-        print ("1. View Tasks")
+        print ("\n1. View Tasks")
         print ("2. Add Task")
         print ("3. Remove Task")
         print ("4. Exit")
 
         try:
-            choice = int(input("Choose an option: "))
+            choice = int(input("\nChoose an option: "))
         except ValueError:
             print("INVALID INPUT. Please enter in a valid choice.")
             continue
         
         match choice:
             case 1:
-
+                length = len(tasks)
+                if length <= 0:
+                    print("There are no tasks.")
+                    length = 0
+                else:                    
+                    for i in range(length):
+                        print(f"{i + 1}. {tasks[i]}")
+            case 2:
+                tasks.append(input("Enter task to add: "))
+                print("Task added.")
+            case 3:
+                delete = input("Enter task to remove: ")
+                if delete in tasks:
+                    tasks.remove(delete)
+                    print("Task removed.")
+            case 4:
+                print("Goodbye!")
+                break
+            case _:
+                print("INVALID INPUT. Please enter in a valid choice.")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExiting...")
